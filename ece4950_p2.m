7bbc521dec7df7dc0cd855834f57189e69046644
Griffin Dube <gdube@g.clemson.edu>
2020-10-11 13:23:21 -0400

    Adding testing for relative angle functions

--- ece4950_p2.m
+++ ece4950_p2.m
@@ -1,6 +1,7 @@
 %Griffin Dube
 %ECE 4950
 %Project 2: Image Processing
+%For angles: negative means cw, positive means ccw
 clear; clc; close all; clear('cam');
 
 % cam = setup_camera(1);
@@ -18,9 +19,22 @@ imshow(img)
 hold on;
 
 items = size(STATS);
-for i = 1:3:4
+angles = zeros(items);
+ang = zeros(items);
+for i = 1:items
     plot(STATS(i).Centroid(1),STATS(i).Centroid(2),'yO','MarkerFaceColor','y');
+    text(STATS(i).Centroid(1),STATS(i).Centroid(2),sprintf("%0.0f",i))
+    ang(i) = get_angle([height/2,width/2],STATS(i).Centroid);
+end
+for i = 1:items
+    for j = 1:items
+        angles(i,j) = get_rel_angle(ang(i),ang(j));
+    end
 end
 plot(height/2,width/2,'yO','MarkerFaceColor','y');
-angle = get_rel_angle([height/2,width/2],STATS(1).Centroid,STATS(4).Centroid);
+fprintf("%f %f\n",angles,ang);
 hold off;
+
+w1 = input("Input Well 1: ");
+w2 = input("Input Well 2: ");
+fprintf("The angle between these wells is:  %0.0f\n",angles(w1,w2));
