f736d088790bc8bf97e8b569a73952dbe22d74dc
Griffin Dube <gdube@g.clemson.edu>
2020-10-11 13:11:10 -0400

    Adding functions to find angle between wells

---
+++ get_angle.m
@@ -0,0 +1,13 @@
+function [angle] = get_angle(center,centroid)
+%get_rel_angle Find the angle between well and X axis
+%   Returns angle relative to -X axis.
+ diff = center - centroid;
+
+ angleRad = atan2(diff(2),diff(1));
+ angle = rad2deg(angleRad);
+ angle = round(angle,0);
+ if (angle < 0)
+     angle = angle + 360;
+ end
+end
+
--- get_rel_angle.m
+++ get_rel_angle.m
@@ -1,14 +1,22 @@
-function [angle] = get_rel_angle(center,centroid1,centroid2)
+function [relAngle] = get_rel_angle(a1,a2)
 %get_rel_angle Find the angle between two wells
-%   Returns clockwise angle between two centers
- P_1 = [centroid1(1),centroid1(2)];
- P_2 = [centroid2(1),centroid2(2)];
- radius = centroid1(1) - center(1);
- 
- dist = pdist([P_1;P_2],'euclidean');
- cosFormula = (2*(radius)^2 - dist^2)/(2*(radius)^2);
- 
- angleRad = acos(cosFormula);
- angle = rad2deg(angleRad);
+%   Returns angle between two centers
+    
+if (a1 < 0)
+    a1 = a1 + 360;
+end
+if (a2 < 0)
+    a2 = a2 + 360;
+end
+
+if (a2 > a1)
+    relAngle = a2 - a1;
+elseif (a1 >= a2)
+    relAngle = a1 - a2;
+end
+
+if (relAngle > 180)
+    relAngle = relAngle - 360;
+
 end
 
