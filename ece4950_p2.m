39887e76932a5ef2bbe20b00e5ddad3b6c7a7b3d
Griffin Dube <gdube@g.clemson.edu>
2020-10-12 15:32:12 -0400

    Adding main file for looping detection

--- ece4950_p2.m
+++ ece4950_p2.m
@@ -1,40 +1,12 @@
 %Griffin Dube
-%ECE 4950
-%Project 2: Image Processing
-%For angles: negative means cw, positive means ccw
-clear; clc; close all; clear('cam');
+%ece4950_p2
 
-% cam = setup_camera(1);
-
-%get background and image
-bg  = imread('washersbgnd.png');
-img = imread('washers.png');%snapshot(cam);
-
-[nobg,height,width,depth] = subtract_background(img,bg);
-bin = cvt_binary_img(nobg,height,width);
-
-STATS = regionprops(bin, 'all');
-figure();
-imshow(img)
-hold on;
-
-items = size(STATS);
-angles = zeros(items);
-ang = zeros(items);
-for i = 1:items
-    plot(STATS(i).Centroid(1),STATS(i).Centroid(2),'yO','MarkerFaceColor','y');
-    text(STATS(i).Centroid(1),STATS(i).Centroid(2),sprintf("%0.0f",i))
-    ang(i) = get_angle([height/2,width/2],STATS(i).Centroid);
+clear;close all;
+[cam,background] = setup_camera(2);
+% background = bg;
+nColors = 4; %Red,Yellow,Green,Blue
+nWells = 8;
+input("Hit Enter to Begin...");
+while 1
+    state = get_current_state(cam,background,nColors,nWells);
 end
-for i = 1:items
-    for j = 1:items
-        angles(i,j) = get_rel_angle(ang(i),ang(j));
-    end
-end
-plot(height/2,width/2,'yO','MarkerFaceColor','y');
-fprintf("%f %f\n",angles,ang);
-hold off;
-
-w1 = input("Input Well 1: ");
-w2 = input("Input Well 2: ");
-fprintf("The angle between these wells is:  %0.0f\n",angles(w1,w2));
