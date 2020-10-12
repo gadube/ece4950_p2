c0be6d8fddc88824501b9182246dc659c53ecd5a
Griffin Dube <gdube@g.clemson.edu>
2020-10-12 12:42:29 -0400

    updating camera setup

--- setup_camera.m
+++ setup_camera.m
@@ -1,10 +1,15 @@
-function [cam] = setup_camera(i)
+function [cam,bg] = setup_camera(i)
 %setup_camera Script to set up camera
 %   Sets up first webcam from position "i" in the webcamlist
 cam_list = webcamlist;
 cam_name = cam_list{i};
 cam = webcam(cam_name);
-%preview(cam);
-%closePreview(cam);
+preview(cam);
+%Setup bg
+input("Capturing Snapshot of Background (Remove all stickers)...");
+bg = snapshot(cam);
+figure();
+imshow(bg);
+closePreview(cam);
 end
 
